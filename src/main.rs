0632e5ac1f4ce5c69a98432c397c3a16374e264d
Justin Noah <justinnoah@gmail.com>
2015-01-07 15:07:11 -0800

    move the chip8 struct to a separate mod

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,18 @@
+// externs
 extern crate sdl;
 
+// uses
 use std::rand::Rng;
 
 use sdl::event::{Event, Key};
 use sdl::video::{SurfaceFlag, VideoFlag};
 
-struct Chip8Unit {
-    /*
-     * 0x000-0x1FF - Chip 8 interpreter (contains font set in emu)
-     * 0x050-0x0A0 - Used for the built in 4x5 pixel font set (0-F)
-     * 0x200-0xFFF - Program ROM and work RAM
-     */
-    memory:    [u8; 4096],
+use chip8::Chip8;
 
-    /*
-     * CPU registers: The Chip 8 has 15 8-bit general purpose registers
-     * named V0,V1 up to VE. The 16th register is used  for the ‘carry flag’.
-     */
-    registers: [u8; 16],
+// mods
+mod chip8;
 
-    // Index register
-    i: u16,
-
-    // program counter
-    pc: u16,
-
-    // Screen - 64 x 32 pixels
-    graphics: [u8; 64*32],
-
-    // No timers, but 60hz counters
-    delay_timer: u8,
-    sound_timer: u8,
-
-    // Stack needed to jump to certain addresses or call subroutines
-    // The chip8 stack has 16 levels
-    stack_pointer: u16,
-    stack: [u16; 16],
-
-    // Chip8 keypad had 16 keys - keep track of the kb state
-    keypad: [u8; 16],
-}
+// Finally, some code
 
 fn init_sdl() {
     // Initialize SDL Video
