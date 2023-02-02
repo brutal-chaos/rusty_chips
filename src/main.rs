afc72b5ec145df2239b9ed630d4f8958fd699893
Justin Noah <justinnoah@gmail.com>
2023-02-01 23:26:58 -0800

    begin an async tokio runtime design pattern

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,60 @@
 /// Copyright 2015-2023, Justin Noah <justinnoah at gmail.com>, All Rights Reserved
-// externs
-extern crate sdl;
+use std::sync::{Arc, RwLock};
+use std::time::Duration;
 
-// uses
-use std::default::Default;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 
-use sdl::event::{Event, Key};
-use sdl::video::{SurfaceFlag, VideoFlag};
+pub(crate) mod chip8;
 
-use chip8::Chip8;
+async fn gui_loop(alive_lock: Arc<RwLock<bool>>) {
+    let sdl_context = sdl2::init().unwrap();
+    let video_sub = sdl_context.video().unwrap();
+    let window = video_sub
+        .window("Rusty Chips", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
 
-// mods
-mod chip8;
-
-/* Finally, some code */
-
-fn init_sdl() {
-    // Initialize SDL Video
-    sdl::init([sdl::InitFlag::Video].as_slice());
-
-    // Give our window a title
-    sdl::wm::set_caption("rusty_chips a Chip8 emulator", "rusty_chips");
-
-    // Initialize the screen
-    let screen = match sdl::video::set_video_mode(
-        800,
-        600,
-        32,
-        [SurfaceFlag::HWSurface].as_slice(),
-        [VideoFlag::DoubleBuf].as_slice(),
-    ) {
-        Ok(screen) => screen,
-        Err(err) => panic!("Failed to set video mode: {}", err),
-    };
-}
-
-fn init_chip8() -> chip8::Chip8 {
-    let mut vm = Chip8 {
-        ..Default::default()
-    };
-
-    // Fontset
-    let fontset = [
-        0xF0u8, 0x90, 0x90, 0x90, 0xF0, // 0
-        0x20, 0x60, 0x20, 0x20, 0x70, // 1
-        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-        0xF0, 0x80, 0xF0, 0x80, 0x80, // F
-    ];
-
-    for c in range(0, fontset.len()) {
-        vm.memory[c] = fontset[c];
-    }
-
-    vm
-}
-
-fn main() {
-    // Initialize the Chip8
-    let mut vm = init_chip8();
-
-    // Initialize SDL
-    init_sdl();
-
-    // SDL main loop
-    'main: loop {
-        'event: loop {
-            match sdl::event::poll_event() {
-                Event::Quit => break 'main,
-                Event::None => break 'event,
-                Event::Key(k, _, _, _) if k == Key::Escape => break 'main,
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        canvas.present();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    let mut alive = alive_lock.write().unwrap();
+                    *alive = false;
+                    break 'running;
+                }
                 _ => {}
             }
         }
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
+}
 
-    // Quit
-    sdl::quit();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let alive_lock = Arc::new(RwLock::new(true));
+    let disp_alive = alive_lock.clone();
+    let disp_task = tokio::spawn(gui_loop(disp_alive));
+    let mut tasks = Vec::with_capacity(2);
+    tasks.push(disp_task);
+    for task in tasks {
+        task.await.unwrap();
+    }
+    Ok(())
 }
